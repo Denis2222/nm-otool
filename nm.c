9d9480f54f1014e5e9f47615a8c37b91093a7ff1
Denis MOUREU-LARRANG <dmoureu-@e1r12p9.42.fr>
2017-03-30 02:57:44 +0200

    ok

--- nm.c
+++ nm.c
@@ -2,46 +2,55 @@
 #include <sys/mman.h>
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
+#include <mach-o/ranlib.h>
+#include <mach-o/fat.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-
 
 void	print_output(struct symtab_command *sym, char *ptr)
 {
 	char			*strtable;
 	struct nlist_64	*array;
-	int				i;
+	unsigned int	i;
 
 	i = 0;
 	array = (void*)ptr+sym->symoff;
 	strtable = (void*)ptr+sym->stroff;
+	printf("\n============%#x=================\nOutput symtab_command:\n", array[i].n_type);
 	while (i<sym->nsyms)
 	{
-		printf("%s %d \n", strtable + array[i].n_un.n_strx, sym->cmd);
+		//printf("name:%s sym->cmd:%d  n_type:%#x n_value:%0.16llx \n", strtable + array[i].n_un.n_strx, sym->cmd, (array[i].n_type & N_TYPE), array[i].n_value);
+		if ((array[i].n_type & N_TYPE) == N_UNDF)
+			printf("                 %s  \n", strtable + array[i].n_un.n_strx);
+		else if ((array[i].n_type & N_TYPE) == N_SECT)
+			printf("%0.16llx %s\n", array[i].n_value, strtable + array[i].n_un.n_strx);
 		i++;
 	}
 }
 
 void	handle_64(char *ptr)
 {
-	int 					ncmds;
-	int						i;
-	struct mach_header_64	*header;
+	unsigned int			i;
+	struct mach_header_64	*mh;
 	struct load_command		*lc;
 	struct symtab_command	*sym;
 
-	header = (struct mach_header_64 *) ptr;
-	ncmds = header->ncmds;
-
-	lc = (void*)(ptr + sizeof(*header));
-	while (i < ncmds)
+	mh = (struct mach_header_64 *) ptr;
+	printf("==================\ninfo mach_header:\ncputype:%d\nfiletype:%d\n",mh->cputype, mh->filetype);
+	i = 0;
+	lc = (void*)(ptr + sizeof(*mh));
+	while (i < mh->ncmds)
 	{
 		if (lc->cmd == LC_SYMTAB)
 		{
 			sym = (struct symtab_command *)lc;
 			print_output(sym, ptr);
 			break;
+		}
+		else
+		{
+			printf("]]]]]]]]]]]]]]]load_command->cmd:%#x\n", lc->cmd);
 		}
 		lc = (void *) lc + lc->cmdsize;
 		i++;
@@ -50,13 +59,17 @@ void	handle_64(char *ptr)
 
 void nm(char *ptr)
 {
-	int magic_number;
+	unsigned int	magic_number;
 
-	magic_number = *(int *) ptr;
+	magic_number = *(unsigned int *) ptr;
+	printf("MAGIC_NUMBER:%#x \n", magic_number);
 	if (magic_number == MH_MAGIC_64)
 	{
 		handle_64(ptr);
 		puts("Je suis un binaire 64 bits");
+	} else if (magic_number == FAT_MAGIC_64)
+	{
+		
 	}
 }
 
